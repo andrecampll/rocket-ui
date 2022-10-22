33c2c7e53b1a6c31168993deec88aadb80a1365d
andrecampll <andrevictor50@gmail.com>
2022-10-22 12:25:00 -0300

    feat: adds Button component ⚡️

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,93 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  minWidth: 120,
+  boxSizing: 'border-box',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+  padding: '0 $4',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  transition: 'all 0.2s',
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$primary500',
+
+        '&:not(:disabled):hover': {
+          background: '$primary300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$primary300',
+        border: '2px solid $primary300',
+
+        '&:not(:disabled):hover': {
+          background: '$primary300',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export type ButtonProps = {
+  as?: ElementType
+} & ComponentProps<typeof Button>
